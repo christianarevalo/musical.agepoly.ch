4f18b53db36778bdc8fa7f0b12e8b718eccdc20d
Christian Arevalo <contact@christianarevalo.ch>
2016-06-16 21:58:19 +0200

    Change view home location

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -8,7 +8,7 @@
                     <div class="panel-heading">Dashboard</div>
 
                     <div class="panel-body">
-                        You are logged in!
+                        <h1>Home</h1>
                         {{ locale() }}
                     </div>
                 </div>
